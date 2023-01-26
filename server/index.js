1a5acac89942d3bc4968f3bacccde7af35855cce
vkasyan76 <vkasyan@gmx.de>
2023-01-26 18:38:31 +0100

    mongo setup

--- server/index.js
+++ server/index.js
@@ -5,6 +5,10 @@ import cors from 'cors'
 import dotenv from 'dotenv'
 import helmet from 'helmet'
 import morgan from 'morgan'
+import clientRoutes from './routes/client.js'
+import generalRoutes from './routes/general.js'
+import managementRoutes from './routes/management.js'
+import salesRoutes from './routes/sales.js'
 
 /* CONFIGURATION */
 dotenv.config()
@@ -22,3 +26,17 @@ app.use('/client', clientRoutes)
 app.use('/general', generalRoutes)
 app.use('/management', managementRoutes)
 app.use('/sales', salesRoutes)
+
+/* MONGOOSE SETUP */
+const PORT = process.env.PORT || 9000
+    // You can also remove the warning by setting the strictQuery to false. This will suppress the warning and will not affect the current behavior of your queries. (chatGPT)
+mongoose.set('strictQuery', false)
+mongoose
+    .connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        app.listen(PORT, () => console.log(`Server Port: ${PORT}`))
+    })
+    .catch((error) => console.log(`${error} did not connect`))
