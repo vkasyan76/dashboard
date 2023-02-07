e95717e758ed3810cf43d8278c5b2f2c31b8a465
vkasyan76 <vkasyan@gmx.de>
2023-02-07 19:37:41 +0100

    Loading Dummy User Data to Mongo DB

--- server/index.js
+++ server/index.js
@@ -10,6 +10,10 @@ import generalRoutes from './routes/general.js'
 import managementRoutes from './routes/management.js'
 import salesRoutes from './routes/sales.js'
 
+// data imports
+import User from './models/User.js'
+import { dataUser } from './data/index.js'
+
 /* CONFIGURATION */
 dotenv.config()
 const app = express()
@@ -29,14 +33,17 @@ app.use('/sales', salesRoutes)
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 9000
-    // You can also remove the warning by setting the strictQuery to false. This will suppress the warning and will not affect the current behavior of your queries. (chatGPT)
+// You can also remove the warning by setting the strictQuery to false. This will suppress the warning and will not affect the current behavior of your queries. (chatGPT)
 mongoose.set('strictQuery', false)
 mongoose
-    .connect(process.env.MONGO_URL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    })
-    .then(() => {
-        app.listen(PORT, () => console.log(`Server Port: ${PORT}`))
-    })
-    .catch((error) => console.log(`${error} did not connect`))
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`))
+
+    // add the data: ONLY ADD THE DATA ONE TIME
+    // User.insertMany(dataUser)
+  })
+  .catch((error) => console.log(`${error} did not connect`))
