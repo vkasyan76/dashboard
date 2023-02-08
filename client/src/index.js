854f6c42f072c885fb170c3141fe6904b6ce8689
vkasyan76 <vkasyan@gmx.de>
2023-02-08 01:25:21 +0100

    Set up for making API calls form Backend

--- client/src/index.js
+++ client/src/index.js
@@ -5,18 +5,23 @@ import App from './App'
 import { configureStore } from '@reduxjs/toolkit'
 import globalReducer from 'state'
 import { Provider } from 'react-redux'
+import { setupListeners } from '@reduxjs/toolkit/query'
+import { api } from 'state/api'
 
 const store = configureStore({
   reducer: {
     global: globalReducer,
+    [api.reducerPath]: api.reducer,
   },
+  middleware: (getDefault) => getDefault().concat(api.middleware),
 })
+setupListeners(store.dispatch)
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
-    </Provider>
+    </Provider>{' '}
   </React.StrictMode>,
 )
--- client/src/scenes/layout/index.jsx
+++ client/src/scenes/layout/index.jsx
@@ -4,10 +4,18 @@ import { Outlet } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 import Navbar from 'components/Navbar'
 import Sidebar from 'components/Sidebar'
+import { useGetUserQuery } from 'state/api'
 
 const Layout = () => {
   const isNonMobile = useMediaQuery('(min-width: 600px)')
   const [isSidebarOpen, setIsSidebarOpen] = useState(true)
+  const userId = useSelector((state) => state.global.userId)
+  const { data } = useGetUserQuery(userId)
+  console.log('data', data)
+  // console.log(
+  //   `getUser URL: ${process.env.REACT_APP_BASE_URL}/General/user/${userId}`,
+  // )
+
   return (
     <Box display={isNonMobile ? 'flex' : 'block'} width="100%" hight="100%">
       <Sidebar
