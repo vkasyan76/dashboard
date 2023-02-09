4600ae1c40c35dc6ff22d060eb1556b670680750
vkasyan76 <vkasyan@gmx.de>
2023-02-09 23:48:24 +0100

    Setting Product & ProductStats in the Backend

--- server/index.js
+++ server/index.js
@@ -12,7 +12,9 @@ import salesRoutes from './routes/sales.js'
 
 // data imports
 import User from './models/User.js'
-import { dataUser } from './data/index.js'
+import Product from './models/Product.js'
+import ProductStat from './models/ProductStat.js'
+import { dataUser, dataProduct, dataProductStat } from './data/index.js'
 
 /* CONFIGURATION */
 dotenv.config()
@@ -45,5 +47,7 @@ mongoose
 
     // add the data: ONLY ADD THE DATA ONE TIME
     // User.insertMany(dataUser)
+    // Product.insertMany(dataProduct)
+    // ProductStat.insertMany(dataProductStat)
   })
   .catch((error) => console.log(`${error} did not connect`))
