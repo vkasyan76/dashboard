fc05073041ee649d503e5f7decebdd162e9fc68f
vkasyan76 <vkasyan@gmx.de>
2023-02-18 12:51:03 +0100

    Geography Frontend

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ import Dashboard from 'scenes/dashboard'
 import Products from 'scenes/products'
 import Customers from 'scenes/customers'
 import Transactions from 'scenes/transactions'
+import Geography from 'scenes/geography'
 
 function App() {
   const mode = useSelector((state) => state.global.mode)
@@ -22,15 +23,16 @@ function App() {
           <CssBaseline />
           <Routes>
             <Route element={<Layout />}>
-              <Route path="/" element={<Navigate to="/dashboard" replace />} />{' '}
-              <Route path="/dashboard" element={<Dashboard />} />{' '}
-              <Route path="/products" element={<Products />} />{' '}
-              <Route path="/customers" element={<Customers />} />{' '}
-              <Route path="/transactions" element={<Transactions />} />{' '}
-            </Route>{' '}
-          </Routes>{' '}
-        </ThemeProvider>{' '}
-      </BrowserRouter>{' '}
+              <Route path="/" element={<Navigate to="/dashboard" replace />} />
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/products" element={<Products />} />
+              <Route path="/customers" element={<Customers />} />
+              <Route path="/transactions" element={<Transactions />} />
+              <Route path="/geography" element={<Geography />} />
+            </Route>
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
   )
 }
