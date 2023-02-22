04c596eee17c42a3db9b0a638dcd803ea4b421d4
vkasyan76 <vkasyan@gmx.de>
2023-02-22 22:02:37 +0100

    Overview Chart Backend

--- server/index.js
+++ server/index.js
@@ -15,11 +15,13 @@ import User from './models/User.js'
 import Product from './models/Product.js'
 import ProductStat from './models/ProductStat.js'
 import Transaction from './models/Transaction.js'
+import OverallStat from './models/OverallStat.js'
 import {
   dataUser,
   dataProduct,
   dataProductStat,
   dataTransaction,
+  dataOverallStat,
 } from './data/index.js'
 
 /* CONFIGURATION */
@@ -71,5 +73,6 @@ mongoose
     //     console.log(`Inserted ${docs.length} documents into the collection`)
     //   }
     // })
+    // OverallStat.insertMany(dataOverallStat)
   })
   .catch((error) => console.log(`${error} did not connect`))
